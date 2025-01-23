49fb3bb13dccc4fe173ebbc458d1fd081a409d1e
justaLoli <huangziyu43@gmail.com>
2025-01-23 18:31:24 +0800

    group done

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+import {
+        DemoMessages,
+        SourceDemoParser,
+} from "https://unpkg.com/@nekz/sdp@0.10.0/esm/src/mod.js";
+import {
+    SarTimer,
+    SourceTimer,
+} from "https://unpkg.com/@nekz/sdp@0.10.0/esm/src/speedrun/mod.js";
+
+// Util
+const formatTime = (time) => {
+    let sec = Math.floor(time);
+    const ms = Math.round((time - sec) * 1000).toString().padStart(3,"0");
+        // .toString()
+        // .padStart(6, "0")
+        // .slice(0, 3);
+
+    if (sec >= 60) {
+        let min = sec / 60;
+        sec = sec % 60;
+        if (min >= 60) {
+            let hrs = min / 60;
+            min = Math.floor(min % 60);
+            return `${Math.floor(hrs)}:${min.toString().padStart(2, "0")}:${
+                sec.toString().padStart(2, "0")
+            }.${ms}`;
+        }
+        return `${Math.floor(min)}:${sec.toString().padStart(2, "0")}.${ms}`;
+    }
+    return `${Math.floor(sec)}.${ms}`;
 };
-import { SourceDemoParser, } from "https://unpkg.com/@nekz/sdp@0.10.0/esm/src/mod.js";
+
 const dropZone = document.getElementById("drop-zone");
-const sortButton = document.getElementById("sort-btn");
+const testButton = document.getElementById("btn");
 const fileTableBody = document.querySelector("#file-table tbody");
+const fileTableHead = document.querySelector("#file-table thead");
 // 存储文件数据
-let fileList = [];
+// {file:demo, mapName: undefined, playbackTime:undefined, playbackTicks:undefined, parsed:False}
+const fileList = [];
+
+const groupedFileList = [];
+
+
 // 排序文件列表，按数字顺序排序
-const sortFiles = (files) => {
-    return files.sort((a, b) => {
+const sortFiles = () => {
+    fileList.sort((a, b) => {
+        console.log(a);
         const matchA = a.file.name.match(/_(\d+)\.dem$/);
         const matchB = b.file.name.match(/_(\d+)\.dem$/);
         if (matchA && matchB) {
@@ -30,73 +59,204 @@ const sortFiles = (files) => {
         return a.file.name.localeCompare(b.file.name);
     });
 };
+const clearTable = () => {
+    fileTableBody.innerHTML = '';
+    fileTableHead.innerHTML = '';
+};
+const createTableTitle = (titles) => {
+    const headerRow = document.createElement("tr");
+    titles.forEach( title => {
+        const headerCell = document.createElement("th");
+        headerCell.textContent = title;
+        headerRow.appendChild(headerCell);
+    });
+    fileTableHead.appendChild(headerRow);
+};
 // 更新表格显示
 const updateTable = () => {
-    fileTableBody.innerHTML = ''; // 清空表格
-    fileList.forEach(({ file, demoInfo }) => {
+    clearTable(); // 清空表格
+    createTableTitle(["Split Name", "Split Time (Seconds)", "Demo Files"])
+    groupedFileList.forEach( group => {
         const row = document.createElement("tr");
         const nameCell = document.createElement("td");
-        nameCell.textContent = file.name;
-        const mapCell = document.createElement("td");
-        mapCell.textContent = demoInfo.mapName;
+        nameCell.textContent = group.splitName;
         const timeCell = document.createElement("td");
-        timeCell.textContent = demoInfo.playbackTime.toString();
-        const tickCell = document.createElement("td");
-        tickCell.textContent = demoInfo.playbackTicks.toString();
+        timeCell.textContent = (Math.round(group.sumTick / 60 * 1000) / 1000).toString();
+        const demosCell = document.createElement("td");
+        demosCell.innerHTML = group.files.map(file => file.file.name).join("<br>");
         row.appendChild(nameCell);
-        row.appendChild(mapCell);
         row.appendChild(timeCell);
-        row.appendChild(tickCell);
+        row.appendChild(demosCell);
         fileTableBody.appendChild(row);
+    } );
+    // createTableTitle(["File Name", "Map Name", "Playback Time", "Playback Time (Seconds)", "Playback Ticks"]);
+    // fileList.forEach( file => {
+    //     const row = document.createElement("tr");
+    //     const nameCell = document.createElement("td");
+    //     nameCell.textContent = file.file.name;
+    //     const mapCell = document.createElement("td");
+    //     mapCell.textContent = file.mapName;
+    //     const timeCell = document.createElement("td");
+    //     timeCell.textContent = formatTime(file.playbackTime);
+    //     const timesecCell = document.createElement("td");
+    //     timesecCell.textContent = file.playbackTime.toString();
+    //     const tickCell = document.createElement("td");
+    //     tickCell.textContent = file.playbackTicks.toString();
+    //     row.appendChild(nameCell);
+    //     row.appendChild(mapCell);
+    //     row.appendChild(timeCell);
+    //     row.appendChild(timesecCell);
+    //     row.appendChild(tickCell);
+    //     fileTableBody.appendChild(row);
+    // });
+};
+
+const parser = SourceDemoParser.default();
+const speedrunTimer = SourceTimer.default();
+const sarTimer = SarTimer.default();
+const tryParseDemo = (ev, fullAdjustment = true) => {
+    let demo = null;
+    try {
+        demo = parser.parse(ev.target.result);
+        // should not important
+        // demo.fileInfo = ev.target.source;
+
+        // Fix message ticks
+        demo.detectGame().adjustTicks();
+
+        // Split screen index
+        // should not important
+        // slots = demo.messages.find((msg) => msg.slot === 1) ? 2 : 1;
+
+        if (fullAdjustment) {
+            // Fix header
+            demo.adjustRange();
+
+            // Adjust 0-tick demos manually
+            if (demo.playbackTicks === 0) {
+                const ipt = demo.getIntervalPerTick();
+                demo.playbackTicks = 1;
+                demo.playbackTime = ipt;
+            } else {
+                // Speedrun rules apply here
+                demo.speedrun = speedrunTimer.time(demo);
+
+                // Check SAR support
+                demo.sar = sarTimer.time(demo);
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    return demo;
+};
+const parseListFiles = ()=>{
+    return new Promise((resolve)=>{
+        let count=0;
+
+        for (const file of fileList) {
+            if(file.parsed){
+                ++count;
+                continue;
+            }
+            const reader = new FileReader();
+            reader.onload = (ev)=>{
+                const demo = tryParseDemo(ev);
+                if(demo != null){
+                    file.mapName = demo.mapName ?? "unknown";
+                    file.playbackTime = demo.playbackTime ?? 0;
+                    file.playbackTime = Math.round(file.playbackTime * 1000) / 1000;
+                    file.playbackTicks = demo.playbackTicks ?? 0;
+                    file.parsed = true;
+                }
+
+                if(++count === fileList.length){
+                    // tuneDemoTime();
+                    resolve();
+                }
+
+            };
+            reader.readAsArrayBuffer(file.file);
+        }
     });
 };
-// 解析文件并提取 mapName
-const parseDemoFile = (file) => __awaiter(void 0, void 0, void 0, function* () {
-    var _a, _b, _c;
-    try {
-        const arrayBuffer = yield file.arrayBuffer();
-        const demo = SourceDemoParser.default()
-            .parse(arrayBuffer);
-        // Fix message ticks?
-        demo.detectGame().adjustTicks();
-        demo.adjustRange();
-        let mapName = demo.mapName;
-        let playbackTime = demo.playbackTime;
-        let playbackTicks = demo.playbackTicks;
-        // Adjust time and tick.
-        console.log(demo.playbackTime);
-        // 返回包含 mapName, playbackTime 和 playbackTicks 的对象
-        return {
-            mapName: (_a = demo.mapName) !== null && _a !== void 0 ? _a : "Unknown",
-            playbackTime: (_b = demo.playbackTime) !== null && _b !== void 0 ? _b : 0, // 假设 playbackTime 存在，若没有提供默认值 0
-            playbackTicks: (_c = demo.playbackTicks) !== null && _c !== void 0 ? _c : 0, // 假设 playbackTicks 存在，若没有提供默认值 0
-        };
+//this is bad and will not add to correct time :( so forget about it
+const tuneDemoTime = ()=>{
+    let sar_speedrun_offset = 18980;
+    fileList[0].playbackTicks += sar_speedrun_offset;
+    fileList[0].playbackTime = fileList[0].playbackTicks / 60;
+    fileList[0].playbackTime = Math.round(fileList[0].playbackTime * 1000) / 1000;
+    const tube_ride = fileList.find(
+        (element) => element.mapName === "sp_a2_bts6"
+    );
+    if(tube_ride){tube_ride.playbackTime = 51.867;tube_ride.playbackTicks = 3112;}
+    const long_fall = fileList.find(
+        (element) => element.mapName === "sp_a3_00"
+    );
+    if(long_fall){long_fall.playbackTime = 77.767;long_fall.playbackTicks = 4666;}
+};
+const groupFiles = ()=>{
+    let group;
+    const mapSet = new Set();
+    fileList.forEach(file=>{
+        if(!mapSet.has(file.mapName)){
+            mapSet.add(file.mapName);
+            if(group){
+                groupedFileList.push(group);
+            }
+            group = {
+                files: [file],
+                splitName: file.mapName,
+                sumTime: file.playbackTime,
+                sumTick: file.playbackTicks
+            };
+        } else {
+            group.files.push(file);
+            group.sumTime += file.playbackTime;
+            group.sumTick += file.playbackTicks;
+        }
+    });
+    if(group){
+        groupedFileList.push(group);
     }
-    catch (error) {
-        console.error("Error parsing demo file:", file.name, error);
-        return {
-            mapName: "Error",
-            playbackTime: 0,
-            playbackTicks: 0,
-        };
-    }
-});
+    console.log(groupedFileList);
+}
+
 // 处理拖放文件
-dropZone.addEventListener("drop", (event) => __awaiter(void 0, void 0, void 0, function* () {
+dropZone.addEventListener("drop", (event) => {
     event.preventDefault();
     dropZone.classList.remove("dragover");
     const files = Array.from(event.dataTransfer.files);
-    for (const file of files) {
-        const demoInfo = yield parseDemoFile(file);
-        fileList.push({ file, demoInfo });
-    }
-    fileList = sortFiles(fileList);
-    updateTable(); // 更新表格显示
-}));
+    const updatedFiles = files.map(demo => {
+        return {
+            file: demo,
+            mapName: undefined,
+            playbackTime: undefined,
+            playbackTicks: undefined,
+            parsed: false
+        };
+    });
+    fileList.push(...updatedFiles);
+    sortFiles();
+    clearTable();
+    createTableTitle(["Please wait while the demos are parsing..."]);
+    parseListFiles().then(()=>{
+        groupFiles();
+        updateTable(); // 更新表格显示
+        console.log(fileList);
+        const sumPlaybackTime = () => {
+            return fileList.reduce((total, item) => total + (item.playbackTime || 0), 0);
+        };
+        console.log(sumPlaybackTime());
+        console.log(formatTime(sumPlaybackTime()));
+    });
+    // debugger;
+});
 // 处理排序按钮点击
-sortButton.addEventListener("click", () => {
-    fileList = sortFiles(fileList); // 排序文件列表
-    updateTable(); // 更新表格显示
+testButton.addEventListener("click", () => {
+    fileList.length=0;
+    groupedFileList.length = 0;
+    clearTable();
 });
 // 处理拖拽区的 hover 状态
 dropZone.addEventListener("dragover", (event) => {
